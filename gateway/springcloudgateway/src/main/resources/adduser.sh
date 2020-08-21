53da9faf481919b69bc2e367b8ae23894589c558
tgh621 <guohai.tang@bigdatacd.com>
2020-08-21 18:00:28 +0800

    代码合并

--- gateway/springcloudgateway/src/main/resources/adduser.sh
+++ gateway/springcloudgateway/src/main/resources/adduser.sh
@@ -3,4 +3,7 @@ mkdir -p /tmp/linkis/$1
 echo $1=$2 >> ../conf/upf.properties
 su - hdfs << EOF
 hadoop dfs -mkdir -p /user/$1
+hadoop dfs -chown root:hdfs /user/$1
+hadoop dfs -mkdir -p /tmp/linkis/$1
+hadoop dfs -chmod 777 /tmp/linkis/$1
 EOF
